1bf2d95ec889f4a065c39e7d3c5d918b7e4b2133
Ahmed Raza Idrisi <85074543+ahmedrazaidrisii@users.noreply.github.com>
2022-07-13 16:59:35 +0100

    Add files via upload

---
+++ Bulk_links_to_pdf.ps1
@@ -0,0 +1,20 @@
+$sourceFile = "links.txt" # the source file containing the URLs you want to convert
+$destFolder = "D:\ahmed_react\" # converted PDFs will be saved here. Folder has to exist.
+$num = 1
+foreach($link in [System.IO.File]::ReadLines($sourceFile))
+{
+  Write-Output $link $num
+  
+$outfile = $num.ToString() + '.pdf'
+& 'C:\Program Files\Google\Chrome\Application\chrome.exe' --headless --print-to-pdf="$destFolder $outfile" $link
+Start-Sleep -s 3
+$num++
+}
+
+# code for to get all thel links from the website
+# var links = document.getElementsByTagName('a');
+
+# for(var i = 0; i< links.length; i++){
+#   // alert(links[i].href);
+#     console.log(links[i].href)
+# }
